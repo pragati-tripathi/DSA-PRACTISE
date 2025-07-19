f2d976ef858aabde1f35d534d1c3894f53fb917d
pragati-tripathi <119396381+pragati-tripathi@users.noreply.github.com>
2025-07-19 09:24:57 +0530

    Add files via upload

---
+++ conditionals.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using  namespace std;
+/*int main(){
+   char ch;
+   cin>>ch;
+   
+   if(ch>='a' && ch<='z'){
+   	cout<<"Lower Character"<<endl;
+   }
+   else if(ch>='A' && ch<='Z'){                         //print uppercase, lowercase, numebers.
+   	cout<<"Upper Character"<<endl;
+   }
+   else if(ch>='0' && ch<='9'){
+   	cout<<"Numbers"<<endl;
+   }
+   else{
+   	cout<<"Other character"<<endl;
+   }
+   return 0;
+}*/
+
+/*int main(){
+	int n;
+	cin>>n;                 //sum of n number.
+	int i=1;
+	int sum=0;
+	while(i<=n){
+		cout<<i<<endl;
+		sum =sum+i;
+		i=i+1;
+	}
+	cout<<sum<<endl;
+}*/
+
+
