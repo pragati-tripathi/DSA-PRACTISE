e7e6345bb21bd64608c7cc540ccbb8f9413e8af4
pragati-tripathi <119396381+pragati-tripathi@users.noreply.github.com>
2025-07-19 09:23:53 +0530

    Add files via upload

---
+++ Pattern DSA.cpp
@@ -0,0 +1,164 @@
+#include<iostream>
+using namespace std;
+/*int main(){
+	int n;
+	cin>>n;
+	int i=1;
+	while(i<=n){                                    *****
+		int j=1;                                    *****
+		while(j<=n){                                *****
+			cout<<'*';
+			j=j+1;
+		}
+		cout<<endl;
+		i=i+1;
+	}
+}*/
+
+/*int main(){
+	int n;
+	cin>>n;                                 1 2 3 4
+	int i=1;                                1 2 3 4
+	while(i<=n){                            1 2 3 4
+		int j=1;
+		while(j<=n){
+			cout<<j;
+			j=j+1;
+		}
+		cout<<endl;
+		i=i+1;
+	}
+}*/
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+	int n;                           //111
+	cin>>n;                          //222
+	int i=1;                         //333
+	while(i<=n){
+		int j=1;
+		int value= i;
+		while(j<=n){
+			cout<<value;
+			j=j+1;
+		}
+		cout<<endl;
+		i=i+1;
+	}
+}
+*/
+
+/*int main(){
+	int n;
+	cin>>n;
+	int i=1;
+	int count=1;                             //1 2 3 4
+	while(i<=n){                             //5 6 7 8
+		int j=1;                             //9 10 11 12
+		while(j<=n){                        // 13 14 15 16
+			cout<<count<<" ";
+			count=count+1;
+			j=j+1;
+		}
+		cout<<endl;
+		i=i+1;
+	}
+}*/
+
+/*int main(){
+	int n;
+	cin>>n;
+	int row=1;
+	while(row<=n){                            *
+		int column=1;                         * *
+		while(column<=row){                   * * *
+			cout<<'*';                        * * * *
+			column=column+1;
+		}
+		cout<<endl;
+		row=row+1;
+	}
+}*/
+/*
+int main(){
+	int n; 
+	cin>>n;
+	int row=1;
+	while(row<=n){
+		int col=1;             1
+		int count = 1;         1 2
+		while(col<=row){       1 2 3
+			cout<<count;       1 2 3 4
+			count=count+1;
+			col=col+1;
+		}
+	cout<<endl;
+	row=row+1;
+	}
+}*/
+
+/*int main(){
+	int n; 
+	cin>>n;
+	int row=1;
+	while(row<=n){
+		int col=1;            // 1
+		int count = row;       //2 3
+		while(col<=row){       //3 4 5
+			cout<<count;       //4 5 6 7
+			count=count+1;
+			col=col+1;
+		}
+	cout<<endl;
+	row=row+1;
+}
+}
+*/
+
+/*
+int main(){
+	int n;
+	cin>>n;                         //1
+	int i=1;                        //2 3
+	int count = 1;                  //4 5 6
+	while(i<=n){                    //7 8 9 10
+		int j=1;
+		while(j<=i){
+			cout<<count<<" ";
+			count= count+1;
+			j=j+1;
+		}
+		cout<<endl;
+		i=i+1;
+	} 
+}*/
+
+/*
+int main(){
+	int n;             
+	cin>>n;                                 1
+	int i=1;                                2 1
+	while(i<=n){                            3 2 1
+		int j=1;                            4 3 2 1
+		while(j<=i){
+			cout<<i-j+1<<" ";
+			j=j+1;
+		}
+		cout<<endl;
+		i=i+1;
+	}
+	
+}*/
+
+
+
+
+
+
+
+
+
+
+
